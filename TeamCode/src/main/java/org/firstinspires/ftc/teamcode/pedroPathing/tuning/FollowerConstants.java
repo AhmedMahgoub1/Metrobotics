c87285ed3283ffb2d6a31fe65fd57cf52016fcd3
AHMEDMAHGOUB1 <Amansour3455@gmail.com>
2024-12-11 11:04:40 -0500

    Fixed encoder positions
    tuned manually
    
    tuned velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
@@ -30,8 +30,8 @@ public class FollowerConstants {
 
     // This section is for setting the actual drive vector for the front left wheel, if the robot
     // is facing a heading of 0 radians with the wheel centered at (0,0)
-    private static double xMovement = 60.5;
-    private static double yMovement = 30;
+    private static double xMovement = 78;
+    private static double yMovement = 65;
     private static double[] convertToPolar = Point.cartesianToPolar(xMovement, -yMovement);
     public static Vector frontLeftVector = MathFunctions.normalizeVector(new Vector(convertToPolar[0], convertToPolar[1]));
 
